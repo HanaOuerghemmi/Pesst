4757c887496c4c43e5e0d5af4fdfc47568e1fbf3
Med Ali Krifa <daliolimona@gmail.com>
2023-12-20 17:14:29 +0100

    constant

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const greyColor = Colors.grey;
-const blackColor = Color(0xFF38304c); //Colors.black;
+const blackColor = Color(0xFF38304c);
 const whiteColor = Color(0xFFF2F2F2);
 const newblack = Color(0xFF38304c);
 const primaryColor = Color(0xFFD83B9D);
