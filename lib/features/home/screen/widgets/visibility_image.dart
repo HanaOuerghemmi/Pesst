1130420d22348963cdbcbc8a4da45601f477c4ac
Med Ali Krifa <daliolimona@gmail.com>
2023-12-25 09:59:56 +0100

    home & profile widgets

---
+++ lib/features/home/screen/widgets/visibility_image.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:pesst/features/home/screen/widgets/image_widget.dart';
+import 'package:pesst/utils/helper_padding.dart';
+
+class VisibilityImage extends StatelessWidget {
+  final bool visibility;
+  final String image;
+  const VisibilityImage({
+    Key? key,
+    required this.visibility,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        visibility ? mediumPaddingVert : const SizedBox(),
+        visibility
+            ? ImageWidget(
+                imageUrl: image,
+              )
+            : const SizedBox()
+      ],
+    );
+  }
+}
